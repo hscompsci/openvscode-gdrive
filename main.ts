b0cc29e424a77d543b92a7331487ff27d88c3256
Sol Boucher <sboucher@cmu.edu>
2022-06-22 23:18:29 -0400

    main.ts: Shut down a user's VSCode instance 90 seconds after they close their last browser tab

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S deno run --import-map vendor/import_map.json --allow-net --allow-read=. --allow-write=refresh_tokens.json --allow-run=chroot/jail --check
+#!/usr/bin/env -S deno run --import-map vendor/import_map.json --allow-net --allow-read=. --allow-write=refresh_tokens.json --allow-run --check
 
 import { deferred } from "https://deno.land/std@0.136.0/async/mod.ts";
 import { decode, encode } from "https://deno.land/std@0.136.0/encoding/base64.ts";
@@ -13,12 +13,16 @@ import { decodeId } from "./openid.ts";
 import { BACKING_FILE, getToken, setToken } from "./refresh_tokens.ts";
 
 const CONFIG_FILE = "config.json";
+const SHUTDOWN_TIMEOUT_S = 90;
 
 const emailToToken: {[_: string]: string} = {};
 const tokenToEditor: {[_: string]: Editor} = {};
 
 type Editor = {
 	port: number,
+	process: Deno.Process,
+	refCount: number,
+	shutdownHandle?: number,
 	queryString?: string,
 };
 
@@ -36,7 +40,7 @@ async function handler(request: Request): Promise<Response> {
 	if(!token)
 		token = url.searchParams.get("tkn");
 	if(token && Object.hasOwn(tokenToEditor, token))
-		return proxy(request, tokenToEditor[token]);
+		return proxy(request, token);
 
 	if(url.pathname != "/") {
 		url.pathname = "/";
@@ -82,7 +86,7 @@ async function handler(request: Request): Promise<Response> {
 	console.log("User " + email + " logged in" + newUser);
 
 	let vsCodeToken = emailToToken[email];
-	if(vsCodeToken)
+	if(vsCodeToken && Object.hasOwn(tokenToEditor, vsCodeToken))
 		console.log(
 			"Reusing user "
 				+ email
@@ -90,19 +94,21 @@ async function handler(request: Request): Promise<Response> {
 				+ tokenToEditor[vsCodeToken].port
 		);
 	else {
-		const vscode = Deno.run({
+		const process = Deno.run({
 			cmd: ["chroot/jail", "vscode", "-c", "openvscode-drive", "--port", "0"],
 			stdin: "piped",
 			stdout: "piped",
 		});
 		const stdin = new TextEncoder();
-		vscode.stdin.write(stdin.encode(tokens.access_token + "\n"));
-		vscode.stdin.write(stdin.encode(tokens.refresh_token + "\n"));
-		vscode.stdin.write(stdin.encode(expiry.toISOString() + "\n"));
+		process.stdin.write(stdin.encode(tokens.access_token + "\n"));
+		process.stdin.write(stdin.encode(tokens.refresh_token + "\n"));
+		process.stdin.write(stdin.encode(expiry.toISOString() + "\n"));
 
-		const {port, token} = await parsePortAndToken(vscode.stdout);
+		const {port, token} = await parsePortAndToken(process.stdout);
 		tokenToEditor[token] = {
 			port,
+			process,
+			refCount: 0,
 		};
 		emailToToken[email] = token;
 		vsCodeToken = token;
@@ -116,7 +122,8 @@ async function handler(request: Request): Promise<Response> {
 	return Response.redirect(String(url));
 }
 
-function proxy(request: Request, editor: Editor): Promise<Response> {
+function proxy(request: Request, token: string): Promise<Response> {
+	const editor = tokenToEditor[token];
 	const url = new URL(request.url);
 	url.hostname = "localhost";
 	url.port = String(editor.port);
@@ -128,12 +135,38 @@ function proxy(request: Request, editor: Editor): Promise<Response> {
 
 	const serverSocket = new WebSocket(String(url));
 	const {response, socket} = Deno.upgradeWebSocket(request);
+	const shutdown = function() {
+		console.log("Shutting down port " + editor.port + " VSCode instance");
+		delete tokenToEditor[token];
+		editor.process.kill("SIGINT");
+	};
+	const decrRefCount = function() {
+		--editor.refCount;
+		if(editor.refCount == 0) {
+			console.log("Last client closed for port " + editor.port + " VSCode instance");
+			editor.shutdownHandle = setTimeout(shutdown, SHUTDOWN_TIMEOUT_S * 1000);
+		}
+	};
+	const incrRefCount = function() {
+		if(editor.refCount == 0 && editor.shutdownHandle) {
+			console.log("Client reopened for port " + editor.port + " VSCode instance");
+			clearTimeout(editor.shutdownHandle);
+			delete editor.shutdownHandle;
+		}
+		++editor.refCount;
+	};
 
 	const serverBootstrap = deferred();
-	serverSocket.onopen = serverBootstrap.resolve;
+	serverSocket.onopen = function() {
+		incrRefCount();
+		serverBootstrap.resolve();
+	};
 
 	const bootstrap = deferred();
-	socket.onopen = bootstrap.resolve;
+	socket.onopen = function() {
+		incrRefCount();
+		bootstrap.resolve();
+	};
 
 	serverSocket.onmessage = async function(message) {
 		await bootstrap;
@@ -147,10 +180,12 @@ function proxy(request: Request, editor: Editor): Promise<Response> {
 	serverSocket.onclose = function() {
 		socket.onmessage = null;
 		socket.close();
+		decrRefCount();
 	};
 	socket.onclose = function() {
 		serverSocket.onmessage = null;
 		serverSocket.close();
+		decrRefCount();
 	};
 
 	return Promise.resolve(response);
