14e5400d66fdc2e8a6045f256f0638c20ae7f24b
Sol Boucher <sboucher@cmu.edu>
2022-07-07 12:48:34 -0400

    AGPLv3 it

--- oauth.ts
+++ oauth.ts
@@ -1,3 +1,19 @@
+// Copyright (C) 2022, Sol Boucher
+//
+// This file is part of OpenVSCode GDrive.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 // OAuth documentation: https://developers.google.com/identity/protocols/oauth2/web-server
 // OpenID documentation: https://developers.google.com/identity/protocols/oauth2/openid-connect
 
